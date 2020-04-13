362c12de5efd9a872afbefb6e29620634f384c10
jadonn <30273534+jadonn@users.noreply.github.com>
2020-04-12 22:03:26 -0400

    Added index page for posts

--- src/routes/blog/index.json.js
+++ src/routes/blog/index.json.js
@@ -1,8 +1,24 @@
-const contents = [];
+import content from './_content';
+import showdown from 'showdown';
+
 export function get(req, res) {
-	res.writeHead(200, {
+    const conv = new showdown.Converter({metadata: true});
+    const files = content.files();
+    const posts = [];
+    for (const file of files){
+        const rawPost = content.file(file);
+        const html = conv.makeHtml(rawPost);
+        const metadata = conv.getMetadata();
+        const post = {
+                title: metadata.title,
+                slug: metadata.slug
+        };
+        posts.push(post);
+    }
+
+    res.writeHead(200, {
 		'Content-Type': 'application/json'
 	});
 
-	res.end(contents);
+	res.end(JSON.stringify(posts));
 }
