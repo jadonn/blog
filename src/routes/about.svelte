394ac535dddfef75459f67da8984eb3020eeb8c6
jadonn <30273534+jadonn@users.noreply.github.com>
2020-04-12 22:06:32 -0400

    Added description to about page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -4,4 +4,4 @@
 
 <h1>About this site</h1>
 
-<p>This is the 'about' page. There's not much here.</p>
+<p>Hello! My name is Jadon Naas, and this is a personal blog I'm working on to help me learn Svelte, Sapper, and better interface design. I also will write about JavaScript and other technology related topics from time to time as a test of my own knowledge and as a way to help others for whom other existing tutorials on the Internet may not have clicked.</p>
