0bda9d9a736a9dac11881c8b8e0f67545a003c7d
jadonn <30273534+jadonn@users.noreply.github.com>
2020-04-12 22:25:20 -0400

    Updated index component

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,32 @@
 <style>
+	h1, p {
+		text-align: left;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
 	p {
 		margin: 1em auto;
 	}
 
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
 </style>
 
 <svelte:head>
 	<title>Jadon.us</title>
 </svelte:head>
 
-<p>Hello! My name is Jadon Naas, and this is a personal blog I'm working on to help me learn Svelte, Sapper, and better interface design. I also will write about JavaScript and other technology related topics from time to time as a test of my own knowledge and as a way to help others for whom other existing tutorials on the Internet may not have clicked.</p>
+<h1>What is this site?</h1>
+<p>Hello! My name is Jadon Naas, and this is a personal blog I'm working on to help me learn <a href="https://svelte.dev/">Svelte</a>, <a href="https://sapper.svelte.dev/">Sapper</a>, and better interface design. I also will write about JavaScript and other technology related topics from time to time as a test of my own knowledge and as a way to help others for whom other existing tutorials on the Internet may not have clicked.</p>
+<h2>About this site</h2>
+<p>This site is a modified Sapper website. It was based off the Sapper Rollup template provided in the <a href="https://sapper.svelte.dev/docs/">Sapper tutorial</a>. Sapper uses Svelte for the front end components and provides server-side rendering, server-side API endpoints, and other useful features.</p>
